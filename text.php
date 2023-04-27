54aa75890d2cd08ccb0844744d18c60cd12ea859
netIDxz3630 <93728266+netIDxz3630@users.noreply.github.com>
2023-04-27 10:15:22 -0400

    Add files via upload

---
+++ text.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'chatgpt_rewrite.php';
+
+// check if the 'text' parameter was received via POST request
+if(isset($_POST['text'])) {
+    // retrieve the value of the 'text' parameter and store it in a variable called 'name'
+    $text = $_POST['text'];
+    // Call the chatgpt function with the user's input
+    $response = chatgpt($text);
+    // Output the character count and response to the user
+    echo $response;
+} else {
+    // output a message indicating that no text was received
+    echo "No text was received.";
+}
